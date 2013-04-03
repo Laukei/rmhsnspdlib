cb618a626a9c0491bfb7e46c048227354b595f7a
Rob Heath <rob@robheath.me.uk>
2013-04-03 17:51:29 +0100

    least-squares-fitting bugfix

--- least-squares-fitting.py
+++ least-squares-fitting.py
@@ -8,7 +8,9 @@
 import numpy as np
 
 input_fluxes = [15.45842,38.8298,97.53604,244.99946,615.41081,1545.84206,3882.9797,9753.60401,24499.94558,61541.08088,154584.20604,388297.96969,975360.40144,2.45E+006,6.15E+006,1.55E+007,3.88E+007,9.75E+007,2.45E+008,6.15E+008,1.55E+009,3.88E+009,9.75E+009,2.45E+010,6.15E+010]
-experimental_counts = [0,0,0,0,0,1,0,2,2,2,2,3,6,13,56,218,1253,6358,29384,101638,285965,656111,962296,998279,1.01E+006]
+#experimental_counts = [0,0,0,0,0,1,0,2,2,2,2,3,6,13,56,218,1253,6358,29384,101638,285965,656111,962296,998279,1.01E+006]
+experimental_counts = [18,27,32,22,38,32,62,31,24,47,49,69,77,177,452,1029,2566,6988,18063,40308,84666,207935,481233,866281,995885]
+
 
 def calculateCountRate(eta,D,exponent,input_fluxes):
 	f = 1000000 #frequency of the laser
@@ -40,7 +42,7 @@ def generateGridOfResiduals(D_range,eta_range,exponent_range,input_fluxes,experi
 				residual = returnResiduals(calculated_counts,experimental_counts)
 				residuals[d][e].append(residual)
 				if residual < minima[0]:
-					minima[1], minima[2], minima[3] = d, e, x
+					minima[0], minima[1], minima[2], minima[3] = residual, d, e, x
 	return residuals, minima
 
 def checkForConvergence(residuals,minima,D_range,eta_range,exponent_range):
@@ -83,9 +85,13 @@ def checkForConvergence(residuals,minima,D_range,eta_range,exponent_range):
 	print "Exp:\t", exponent_range[x] 
 	print "with minima residual value: ", minima[0]
 
-D_range = np.linspace(0,30,30) #from x, to y, spaced as Z
-eta_range = np.linspace(0,0.01,1001)
-exponent_range = np.linspace(1,2,101)
+#D_range = [2] #from x, to y, spaced as Z
+D_range = np.linspace(10,30,5)
+eta_range = np.linspace(0,0.01,1000)
+exponent_range = np.linspace(0.8,1.4,61)
+#exponent_range = [1]
+#D needs locking in place - the weights at the far end excessively outweigh the near end
 
 residuals, minima = generateGridOfResiduals(D_range,eta_range,exponent_range,input_fluxes,experimental_counts)
 checkForConvergence(residuals,minima,D_range,eta_range,exponent_range)
+
